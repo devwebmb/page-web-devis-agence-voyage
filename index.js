806acc53acdada0a65600e9449d21175b62619f6
Bruno Mahiet <devwebmb@gmail.com>
2022-12-19 10:18:52 +0100

    test radio

---
+++ index.js
@@ -0,0 +1,10 @@
+/*affichage formulaire */
+
+// const acceptDevis = document.getElementById('acceptDevis')
+// const acceptCgv = document.getElementById('acceptCgv')
+
+// acceptDevis.addEventListener('change', () => {
+//     if (acceptDevis.checked) {
+//         console.log("good");
+//     }
+// })
