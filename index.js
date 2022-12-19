bd26906774b624a286b530146c80a7a960a9d9fd
Bruno Mahiet <devwebmb@gmail.com>
2022-12-19 12:14:51 +0100

    suite 19 décembre form

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /*affichage formulaire */
 
-// const acceptDevis = document.getElementById('acceptDevis')
-// const acceptCgv = document.getElementById('acceptCgv')
+const acceptDevis = document.getElementById("acceptDevis");
+const acceptCgv = document.getElementById("acceptCgvContainer");
 
-// acceptDevis.addEventListener('change', () => {
-//     if (acceptDevis.checked) {
-//         console.log("good");
-//     }
-// })
+document.querySelectorAll(`input[type="radio"]`).forEach((elem) => {
+    elem.addEventListener("change", () => {
+        if (acceptDevis.checked) {
+            acceptCgv.classList.remove("d-none");
+        } else if (!acceptDevis.checked) {
+            acceptCgv.classList.add("d-none");
+        };
+    })
+})
