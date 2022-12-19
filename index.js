1cba4990f156d8c212c2be570b148f5b68f69816
Bruno Mahiet <devwebmb@gmail.com>
2022-12-19 13:52:33 +0100

    check Patrick

--- index.js
+++ index.js
@@ -4,11 +4,30 @@ const acceptDevis = document.getElementById("acceptDevis");
 const acceptCgv = document.getElementById("acceptCgvContainer");
 
 document.querySelectorAll(`input[type="radio"]`).forEach((elem) => {
-    elem.addEventListener("change", () => {
-        if (acceptDevis.checked) {
-            acceptCgv.classList.remove("d-none");
-        } else if (!acceptDevis.checked) {
-            acceptCgv.classList.add("d-none");
-        };
-    })
-})
+  elem.addEventListener("change", () => {
+    if (acceptDevis.checked) {
+      acceptCgv.classList.remove("d-none");
+    } else if (!acceptDevis.checked) {
+      acceptCgv.classList.add("d-none");
+    }
+  });
+});
+
+/*affichage CGV */
+
+const displayCgv = document.getElementById("displayCgv");
+const cgvView = document.getElementById("cgvView");
+const afficher = document.getElementById("afficher");
+const cacher = document.getElementById("cacher");
+
+displayCgv.addEventListener("click", () => {
+  if (cgvView.classList.contains("d-none")) {
+    cgvView.classList.remove("d-none");
+    afficher.classList.add("d-none");
+    cacher.classList.remove("d-none");
+  } else if (!cgvView.classList.contains("d-none")) {
+      cgvView.classList.add("d-none");
+        afficher.classList.remove("d-none");
+        cacher.classList.add("d-none");
+  }
+});
